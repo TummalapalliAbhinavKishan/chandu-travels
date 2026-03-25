947e77229ddbe2340b37553c71e0e8687a8dc634
Abhinav Kishan <147715609+TummalapalliAbhinavKishan@users.noreply.github.com>
2026-03-25 20:34:42 +0530

    Remove Supabase credentials console logs
    Removed console logs for Supabase credentials.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE ANON KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-console.log("SUPABASE PUBLISHABLE KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 
 createRoot(document.getElementById("root")!).render(<App />);
